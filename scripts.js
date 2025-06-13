4cca1b93a1615bbb4f65474284c740b1f8060df4
tobiasvidela <tobiasvidelaug14@gmail.com>
2025-06-12 21:23:33 -0300

    navbar changed

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
 const wsp_oficina_clientes = "5493585024891";
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
 
 function openPopup() {
   document.getElementById("popup").style.display = "flex";
